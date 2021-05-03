4c9d7b0ff726562e170617e0a4ff2d1876c63567
m8 <umusasadik@gmail.com>
2021-05-03 18:36:45 +0300

    connection limit added

--- db/conn.js
+++ db/conn.js
@@ -5,7 +5,10 @@ var con = mysql.createConnection({
   host: "localhost",
   user: defaults.db.user,
   password: defaults.db.password,
-  database: "myvideos"
+  database: "myvideos",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 con.connect();
