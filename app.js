2b323abfa3a35b3d37a8855f4681e03f78f89379
m8 <umusasadik@gmail.com>
2021-05-03 17:27:16 +0300

    admin page added

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var videoRouter = require('./routes/add_video');
+var adminRouter = require('./routes/admin');
 
 var con = require('./db/init');
 
@@ -30,6 +31,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/', videoRouter);
 app.use('/users', usersRouter);
+app.use('/admin', adminRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
