d27fc49c96ba2d944785551daaff66256fe0268e
m8 <umusasadik@gmail.com>
2021-05-04 18:35:16 +0300

    insert user - admin panel

--- routes/admin.js
+++ routes/admin.js
@@ -6,10 +6,15 @@ var con = require('../db/con');
 
 /* GET admin page. */
 router.get('/', async function(req, res, next) {
-
     con.query("select * from user",function (err, result) {
         res.render('admin',{users:result});
     }); 
+});
+
+router.post('/add-user', async function(req,res,next){
+    const {name,surname,email,username,password} = req.body;
+    await con.query(`INSERT INTO user SET ?`, {name,surname,email,username,password} , function (err, result) {}); 
+    res.redirect('/admin');
 });
 
 module.exports = router;
