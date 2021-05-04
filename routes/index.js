db46d0cf4e999b448435a56d499e38de6e06848b
m8 <umusasadik@gmail.com>
2021-05-04 18:35:23 +0300

    changes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
-let conn = require('../db/conn');
 
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   let s = 'deneme'
-  res.redirect('/add-video');
+  res.render('index');
 });
+
 
 module.exports = router;
