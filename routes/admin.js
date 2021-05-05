e7107da09f3444072f6fd923e499de4342adf0c2
Hasan Ocak <ocakhasan99@gmail.com>
2021-05-05 13:27:32 +0300

    changed con.js to conn.js and handled the imports from other files

--- routes/admin.js
+++ routes/admin.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var router = express.Router();
 var queries = require('../db/query');
-var con = require('../db/con');
+var con = require('../db/conn');
 
 
 /* GET admin page. */
-router.get('/', async function(req, res, next) {
-    con.query("select * from user",function (err, result) {
-        res.render('admin',{users:result});
-    }); 
+router.get('/', async function (req, res, next) {
+    con.query("select * from user", function (err, result) {
+        res.render('admin', { users: result });
+    });
 });
 
-router.post('/add-user', async function(req,res,next){
-    const {name,surname,email,username,password} = req.body;
-    await con.query(`INSERT INTO user SET ?`, {name,surname,email,username,password} , function (err, result) {}); 
+router.post('/add-user', async function (req, res, next) {
+    const { name, surname, email, username, password } = req.body;
+    await con.query(`INSERT INTO user SET ?`, { name, surname, email, username, password }, function (err, result) { });
     res.redirect('/admin');
 });
 
