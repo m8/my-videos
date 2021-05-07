d105ec71ce9f34a9fc3dd934ccf0def5d451bf59
m8 <umusasadik@gmail.com>
2021-05-07 22:47:13 +0300

    s change

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,12 @@ var express = require('express');
 var router = express.Router();
 var connectionPool = require('../db/conn');
 
-
-
 /* GET home page. */
 router.get('/', async function(req, res, next) {
   if(req.session.user){
+    
     const connection = await connectionPool.getConnection();
-
+    
     const query = "SELECT * FROM (SELECT * FROM user_has_video WHERE user_id = ?) AS W JOIN video on W.video_id = video.id ";
     const [videos] = await connection.execute(query, [req.session.user.id]);
     
@@ -24,8 +23,9 @@ router.get('/login', async function(req, res, next) {
 });
 
 router.post('/login', async function(req,res,next){
-  const connection = await connectionPool.getConnection();
 
+  const connection = await connectionPool.getConnection();
+  
   const { username, password } = req.body;
   const query = "SELECT * FROM user WHERE username=? and password=?";
   const [row] = await connection.execute(query, [username,password]);
@@ -36,6 +36,7 @@ router.post('/login', async function(req,res,next){
   }else{
     res.render('login', {videos: null});
   }
+
 })
 
 
